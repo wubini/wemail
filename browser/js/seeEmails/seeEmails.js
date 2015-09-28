716bc8e9a0e8bfaa960a2af9703299692cda1bff
Kathy Lu <kaffy.lu@gmail.com>
2015-09-27 22:25:13 -0400

    get all emails on see emails page

--- browser/js/seeEmails/seeEmails.js
+++ browser/js/seeEmails/seeEmails.js
@@ -3,5 +3,15 @@ app.config(function ($stateProvider) {
     .state('seeEmails', {
         url: '/seeEmails',
         templateUrl: 'js/seeEmails/seeEmails.html',
+        resolve: {
+          allEmails: function(EmailFactory){
+            return EmailFactory.getAll();
+          }
+        },
+        controller: "SeeEmailsCtrl"
     })
 })
+
+app.controller("SeeEmailsCtrl", ($scope, allEmails) => {
+  $scope.allEmails = allEmails;
+});
