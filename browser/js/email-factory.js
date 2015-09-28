81ffa6f83fb4349e79ec9836b20d229a660b3e22
Aneury Casado <aneury.casado@gmail.com>
2015-09-28 01:52:53 -0400

    Changed emails factory

--- browser/js/email-factory.js
+++ browser/js/email-factory.js
@@ -1,7 +1,7 @@
 app.factory("EmailFactory", function($http){
   return {
     getAll : function(){
-      return $http.get("http://127.0.0.1:1337/api/emails")
+      return $http.get("/api/emails")
       .then(function(response){
         return response.data;
       });
