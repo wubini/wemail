efed254432bad5436f252e3d21c3c55f690a3589
Aneury Casado <aneury.casado@gmail.com>
2015-09-28 00:27:23 -0400

    Made see emails page.

---
+++ browser/js/common/directives/email/email.js
@@ -0,0 +1,6 @@
+app.directive("email", function(){
+  return{
+    restrict: "E",
+    templateUrl: "js/common/directives/email/email.html"
+  }
+})
---
+++ browser/js/common/directives/searchForm/searchForm.js
@@ -0,0 +1,6 @@
+app.directive("searchForm", function(){
+  return {
+    restrict: "E",
+    templateUrl: "js/common/directives/searchForm/searchForm.html"
+  }
+});
